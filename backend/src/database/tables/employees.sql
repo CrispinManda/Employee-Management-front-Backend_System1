0b06e7cf179c7e3d16495c5a1f0a86e733e77409
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-11-05 14:23:58 -0800

    navigate to admin page

--- backend/src/database/tables/employees.sql
+++ backend/src/database/tables/employees.sql
@@ -12,3 +12,5 @@ CREATE TABLE Employees (
     welcomed BIT Default 0
 ) 
 
+SELECT * FROM Employees WHERE email='emmanuel@gmail.com'
+UPDATE Employees SET role = 'admin' WHERE email = 'emmanuel@gmail.com'
